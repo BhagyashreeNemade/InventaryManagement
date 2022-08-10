a95fe893c43d303bed8dc3724f0f8d6a7fffe46e
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-10 19:05:43 +0530

    Stock account Management

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StockAccountManagement
+{
+    class Program
+    {
+     
+        static string stockfilepath = @"C:\Users\admin1\source\repos\StockAccountManagement\StockAccountManagement\Stocklist.json";
+        static void Main(string[] args)
+        {
+            StockMain stocklist = new StockMain();
+            stocklist.DisplayStockData(stockfilepath);
+                      
+        }
+    }
+}
---
+++ StockModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockAccountManagement
+{
+
+    class StockModel
+    {
+        public string stockname { get; set; }
+        public int stocknumber { get; set; }
+        public int stockprice { get; set; }
+    }
+}
