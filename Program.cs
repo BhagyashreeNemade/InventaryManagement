637e81fc1fdf5052227859daa14d231a8e518084
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-10 15:41:10 +0530

    Inventory Details for Rice, Pulses and Wheats with properties name, weight, price per kg.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using InventoryManagement;
+
+namespace Inventarymanagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FetchInventoryDetails fetchInventoryDetails = new FetchInventoryDetails();
+
+
+            string filepath = "C:\\Users\\admin1\\BridgeLab\\c sharp\\InventoryManagement\\InventoryManagement\\data.json";
+
+            Rice data = fetchInventoryDetails.Read(filepath);
+            Console.WriteLine("For Rice");
+            Console.WriteLine("--------------------------");
+
+            for (int i = 0; i < data.typesofRice.Count; i++)
+            {
+
+                Console.WriteLine(data.typesofRice[i].name);
+                Console.WriteLine(data.typesofRice[i].price);
+                Console.WriteLine(data.typesofRice[i].weight);
+                Console.WriteLine("Price : " + data.typesofRice[i].price * data.typesofRice[i].weight);
+
+
+                Console.WriteLine("--------------");
+            }
+        
+             Console.WriteLine("For Pulses");
+             Console.WriteLine("--------------------------"); ;
+             for (int i = 0; i<data.typesofPulses.Count; i++)
+             {
+                Console.WriteLine("Name : " + data.typesofPulses[i].name);
+                Console.WriteLine("Price per Kg : " + data.typesofPulses[i].price);
+                Console.WriteLine("Weight : " + data.typesofPulses[i].weight);
+                Console.WriteLine("Price : " + data.typesofPulses[i].price* data.typesofPulses[i].weight);
+                Console.WriteLine("--------------------------");
+             }
+             Console.WriteLine("For Wheat");
+            Console.WriteLine("--------------------------");
+            for (int i = 0; i<data.typesofWheats.Count; i++)
+            {
+                Console.WriteLine("Name : " + data.typesofWheats[i].name);
+                Console.WriteLine("Price per Kg : " + data.typesofWheats[i].price);
+                Console.WriteLine("Weight : " + data.typesofWheats[i].weight);
+                Console.WriteLine("Price : " + data.typesofWheats[i].price* data.typesofWheats[i].weight);
+                 Console.WriteLine("--------------------------");
+            }
+
+        }
+    }
+}
---
+++ Rice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    public class Rice
+    {
+        public List<TypesofRice> typesofRice;
+        public List<TypesofRice> typesofPulses;
+        public List<TypesofRice> typesofWheats;
+    }
+
+    public class TypesofRice
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesofPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypesofWheats
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+}
