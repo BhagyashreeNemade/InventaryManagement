9d56a6f31dc31d44855383f4e815f6cd3e7534a4
BhagyashreeNemade <108821937+BhagyashreeNemade@users.noreply.github.com>
2022-08-10 17:03:07 +0530

    Extended Inventory Management Program

---
+++ InventoryFactory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement
+{
+    internal class InventoryFactory
+    {
+        public List<InventoryModel> Rice { get; set; }
+        public List<InventoryModel> Wheat { get; set; }
+        public List<InventoryModel> Pulses { get; set; }
+    }
+    public class InventoryModel
+    {
+
+        public string Name { get; set; }
+        public int Weight { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using InventoryManagement;
+
+namespace Inventarymanagement
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nWelcome to Object Oriented Programs");
+                Console.WriteLine("Enter Progran number to get excuted \n1. Inventory management \n2. Extend Inventory data management \n3. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        InventoryMain Data = new InventoryMain();
+                        Data.DisplayData("C:\\Users\\admin1\\BridgeLab\\c sharp\\InventoryManagement\\InventoryManagement\\Inventory.json");
+                        break;
+                    case 2:
+                        InventoryMain manage = new InventoryMain();
+                        manage.ReadData(@"C:\Users\admin1\BridgeLab\c sharp\InventoryManagement\InventoryManagement\Inventory.json");
+                        manage.DisplayData("Rice");
+                        manage.DisplayData("Wheat");
+                        manage.DisplayData("Pulses");
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }
+        }
+    }
+}
